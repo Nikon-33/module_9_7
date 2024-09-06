ec5831d25131c3925006c77ab58d394c185a994e
Nikon-33 <89101717800@mail.ru>
2024-09-06 21:21:16 +0300

    Add files via upload

---
+++ module_9_7.py
@@ -0,0 +1,21 @@
+def is_prime(func):
+    def wrapper(*args):
+        res = func(*args)
+        if res == 0 or res == 1:
+            print("Не простое и не составное")
+        if (res % 2 == 0 and res % res == 0) or (res % 3 == 0 and res % res == 0):
+            print("Составное")
+        else:
+            print("Простое")
+        return res
+
+    return wrapper
+
+
+@is_prime
+def sum_three(*args):
+    return sum(args)
+
+
+result = sum_three(2, 3, 6)
+print(result)
